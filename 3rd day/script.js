662dd8b51e4d4ebb87344b306f81e81a0d90f9ea
GauravVashist239247 <gvashist1010@gmail.com>
2024-10-17 13:56:12 +0530

    first commit

---
+++ 3rd day/script.js
@@ -0,0 +1,100 @@
+// function fon(){
+//     window.alert("Hello world");
+
+//     console.log("Gaurav Vashist");
+// }
+
+// fon();
+
+
+// document.write("swift");
+// let v=5;
+// v=8;
+// console.log(v);
+
+// hey=40;
+// console.log(typeof(hey))
+
+
+// cannot change const value
+// const k=10;
+// k=30
+// console.log(k)
+
+// if(hey===40){
+//     console.log("true")
+// }
+
+// else{
+//     console.log("false")
+// }
+
+
+
+// for(let i=0;i<5;i++){
+//     console.log("Gaurav Vashist")
+// }
+
+
+// for(let i=0;i<10;i++){
+//     console.log("i=",i)
+ 
+
+
+
+// let i=0;
+// while(i<10){
+//    console.log("i'm while loop")
+//    i++
+// }
+
+
+
+
+
+// for(let i=0;i=arr.length;i++){
+    //     let res=arr[i];
+    //     if(res===5){
+        //         console.log("yes")
+        //     }
+//     else{
+//         console.log("no")
+//     }
+// }
+
+
+// }
+
+// console.log(arr);
+
+
+
+
+// const array= [0,"bcs1st year","apple",true,false]
+// console.log(typeof(array[0]))
+// if(5==5){
+    //     const i = 5
+    // }
+    // if(true){
+        //     const i=8;
+        // }
+        // console.log(i);
+        
+        
+        let arr=[1,3,5,6,8]
+        
+    let find=5;
+    let lable=false;
+        for(let i=0;i<arr.length;i++){
+            if(find===arr[i]){
+                lable=true;
+            }
+            
+        }
+
+        if(lable==true){
+            console.log("value is present")
+        }
+        else{
+            console.log("value is not present")
+        }
